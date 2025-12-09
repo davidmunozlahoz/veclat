375a7ad8af9422233723ea09806adbd1399630fc
David Muñoz Lahoz <davidmunozlahoz@gmail.com>
2025-12-09 15:56:21 +0100

    feat: add new files

--- VecLat.lean
+++ VecLat.lean
@@ -1,3 +1,8 @@
 import VecLat.Basic
 import VecLat.VectorOrderIdeal
-import VecLat.PrincipalIdeal
+import VecLat.Hom
+import VecLat.Lattice
+import VecLat.Quotient
+import VecLat.UnitalAMSpace
+import VecLat.Maximal
+import VecLat.Character
